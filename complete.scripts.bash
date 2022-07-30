0cde139dd4f64475e1aadb58d5f4c41b422f7626
IGOR DE PAULA SILVA <dpsigor@gmail.com>
2022-07-30 19:51:43 -0300

    add users table

--- complete.scripts.bash
+++ complete.scripts.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-complete -W "postgres createdb dropdb execdb migrateup migratedown sqlc lint test watch mock" ./scripts.bash
+complete -W "postgres createdb dropdb execdb migrateup migratedown sqlc lint test watch mock migratedown1 migrateup1" ./scripts.bash
